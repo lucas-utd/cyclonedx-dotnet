ab837be6501e02bc23f4e445452fa6066418f20b
Lucas Chen <chen@spectralmd.com>
2025-10-16 10:56:36 -0500

    Create the test cases for update excels from csvs

--- CycloneDX.Tests/ProgramTests.cs
+++ CycloneDX.Tests/ProgramTests.cs
@@ -17,12 +17,15 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
+using CsvHelper;
 using CycloneDX.Interfaces;
 using CycloneDX.Models;
 using Moq;
@@ -177,6 +180,34 @@ namespace CycloneDX.Tests
             Assert.True(File.Exists(@"D:\Test\Bom\SpectralMD.ImagingApp.csv"));
         }
 
+        [Fact]
+        public void UpdateRedExcel_BurnAIAPP()
+        {
+            string csvFilePath = @"D:\Test\Bom\SpectralAI.Burn.csv";
+            Dictionary<string, SbmRecord> csvData = ReadCsv(csvFilePath);
+
+            string tempExcelPath = @"D:\Test\Bom\SpectralAI.Burn.xlsx";
+            string modifiedExcelPath = Path.Combine(@"D:\Test\Bom\", "compare_modified_SpectralAI.Burn.xlsx");
+
+            UpdateExcelFromCsv(csvFilePath, tempExcelPath, modifiedExcelPath);
+
+            Assert.True(File.Exists(modifiedExcelPath));
+        }
+
+        [Fact]
+        public void UpdateRedExcel_ImagingAPP()
+        {
+            string csvFilePath = @"D:\Test\Bom\SpectralMD.ImagingApp.csv";
+            Dictionary<string, SbmRecord> csvData = ReadCsv(csvFilePath);
+
+            string tempExcelPath = @"D:\Test\Bom\SpectralMD.ImagingApp.xlsx";
+            string modifiedExcelPath = Path.Combine(@"D:\Test\Bom\", "compare_modified_SpectralMD.ImagingApp.xlsx");
+
+            UpdateExcelFromCsv(csvFilePath, tempExcelPath, modifiedExcelPath);
+
+            Assert.True(File.Exists(modifiedExcelPath));
+        }
+
 
         // Please try to avoid using var in the below method to make it easier to read
         private static async Task ExportBomToCsv(Bom bom, string csvPath)
@@ -184,7 +215,7 @@ namespace CycloneDX.Tests
             List<string> lines =
             [
                 // Header
-                "PURL,Hashcode,Name,Version,Release Date,Supplier Name,Description,Licenses,Relationship,Level of Support,End of Support Date,External References",
+                "PURL,Hashcode,Name,Version,ReleaseDate,SupplierName,Description,Licenses,Relationship,LevelOfSupport,EndOfSupportDate,ExternalReferences",
             ];
 
             // Build a lookup for dependencies
@@ -323,6 +354,144 @@ namespace CycloneDX.Tests
             }
 
             await File.WriteAllLinesAsync(csvPath, lines);
+        }
+
+
+        private void UpdateExcelFromCsv(string csvFilePath, string excelFilePath, string excelOutputFilePath)
+        {
+            // 1. Read local CSV file
+            Dictionary<string, SbmRecord> csvData = ReadCsv(csvFilePath);
+
+            // 2. Open Excel using ClosedXML
+            HashSet<string> excelPURLs = [];
+            using (XLWorkbook workbook = new(excelFilePath))
+            {
+                IXLWorksheet worksheet = workbook.Worksheet("parsed_bom_burn-ai");  // or choose by name
+
+                // 3. Add scribble and red background for the no-exist in CSV
+                var lastRowUsed = worksheet.LastRowUsed();
+                int maxRow = lastRowUsed != null ? lastRowUsed.RowNumber() : worksheet.RowCount();
+
+                var lastColUsed = worksheet.LastColumnUsed();
+                int maxCol = lastColUsed != null ? lastColUsed.ColumnNumber() : worksheet.ColumnCount();
+
+                // Begin from row 2, assuming row 1 is header
+                for (int r = 2; r <= maxRow; r++)
+                {
+                    string excelKey = worksheet.Cell(r, 1).GetValue<string>().Trim(); // assuming first column is key
+                    excelPURLs.Add(excelKey);
+                    if (csvData.TryGetValue(excelKey, out SbmRecord csvRow))
+                    {
+                        // exists in CSV, do nothing for now
+                        worksheet.Cell(r, 9).SetValue(csvRow.Relationship);
+                        IXLCell cell = worksheet.Cell(r, 9);
+                        cell.Style.Font.FontColor = XLColor.Red;
+                        worksheet.Cell(r, 10).SetValue(csvRow.LevelOfSupport);
+                        cell = worksheet.Cell(r, 10);
+                        cell.Style.Font.FontColor = XLColor.Red;
+                        worksheet.Cell(r, 11).SetValue(csvRow.EndOfSupportDate);
+                        cell = worksheet.Cell(r, 11);
+                        cell.Style.Font.FontColor = XLColor.Red;
+                    }
+                    else
+                    {
+                        // not found in CSV: mark Excel cell text red and strikethrough all cells in this row
+                        for (int c = 1; c <= maxCol; c++)
+                        {
+                            string excelVal = worksheet.Cell(r, c).GetValue<string>().Trim();
+                            if (!string.IsNullOrEmpty(excelVal))
+                            {
+                                // mark all cells in this row red background
+                                IXLCell cell = worksheet.Cell(r, c);
+                                cell.Style.Font.FontColor = XLColor.Red;
+                                // add strikethrough scribble
+                                cell.Style.Font.Strikethrough = true;
+                            }
+                        }
+                    }
+                }
+
+                // 4. (Later) Add new rows for the new entries in CSV
+                foreach (var kvp in csvData)
+                {
+                    if (!excelPURLs.Contains(kvp.Key))
+                    {
+                        // New entry in CSV, add new row at the end
+                        var previousKeyCell = worksheet.Cell(maxRow, 1);
+
+                        var newRow = worksheet.Row(++maxRow);
+                        var (PURL, Hashcode, Name, Version, ReleaseDate, SupplierName, Description, Licenses, Relationship, LevelOfSupport, EndOfSupportDate, ExternalReferences) = kvp.Value;
+                        newRow.Cell(1).Value = PURL;
+                        newRow.Cell(2).Value = Hashcode;
+                        newRow.Cell(3).Value = Name;
+                        newRow.Cell(4).Value = Version;
+                        newRow.Cell(5).Value = ReleaseDate;
+                        newRow.Cell(6).Value = SupplierName;
+                        newRow.Cell(7).Value = Description;
+                        newRow.Cell(8).Value = Licenses;
+                        newRow.Cell(9).Value = Relationship;
+                        newRow.Cell(10).Value = LevelOfSupport;
+                        newRow.Cell(11).Value = EndOfSupportDate;
+                        newRow.Cell(12).Value = ExternalReferences;
+                        // mark the entire new row green background
+                        for (int c = 1; c <= maxCol; c++)
+                        {
+                            IXLCell cell = newRow.Cell(c);
+                            cell.Style.Font.FontColor = XLColor.Red;
+                            // Wrap text for better readability
+                            cell.Style.Alignment.WrapText = true;
+                            // Text alignment middle
+                            cell.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                            // Text alignment left
+                            cell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Left;
+                            // Add thin border
+                            cell.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                            cell.Style.Border.OutsideBorderColor = XLColor.Black;
+                            cell.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+                            cell.Style.Border.InsideBorderColor = XLColor.Black;
+                            if (maxRow % 2 == 0)
+                            {
+                                cell.Style.Fill.SetBackgroundColor(XLColor.LightGray);
+                            }
+                            else
+                            {
+                                cell.Style.Fill.SetBackgroundColor(XLColor.White);
+                            }
+                        }
+                    }
+                }
+
+                // 5. Save modified Excel (locally)
+                workbook.SaveAs(excelOutputFilePath);
+            }
+        }
+
+        private Dictionary<string, SbmRecord> ReadCsv(string csvFilePath)
+        {
+            // Use CsvHelper
+            using var reader = new StreamReader(csvFilePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            List<SbmRecord> records = csv.GetRecords<SbmRecord>().ToList();
+            Dictionary<string, SbmRecord> result = [];
+            foreach (var record in records)
+            {
+                result.Add(record.PURL, record);
+            }
+            return result;
+        }
+    }
+
+    internal record struct SbmRecord(string PURL, string Hashcode, string Name, string Version, string ReleaseDate, string SupplierName, string Description, string Licenses, string Relationship, string LevelOfSupport, string EndOfSupportDate, string ExternalReferences)
+    {
+        public static implicit operator (string PURL, string Hashcode, string Name, string Version, string ReleaseDate, string SupplierName, string Description, string Licenses, string Relationship, string LevelOfSupport, string EndOfSupportDate, string ExternalReferences)(SbmRecord value)
+        {
+            return (value.PURL, value.Hashcode, value.Name, value.Version, value.ReleaseDate, value.SupplierName, value.Description, value.Licenses, value.Relationship, value.LevelOfSupport, value.EndOfSupportDate, value.ExternalReferences);
+        }
+
+        public static implicit operator SbmRecord((string PURL, string Hashcode, string Name, string Version, string ReleaseDate, string SupplierName, string Description, string Licenses, string Relationship, string LevelOfSupport, string EndOfSupportDate, string ExternalReferences) value)
+        {
+            return new SbmRecord(value.PURL, value.Hashcode, value.Name, value.Version, value.ReleaseDate, value.SupplierName, value.Description, value.Licenses, value.Relationship, value.LevelOfSupport, value.EndOfSupportDate, value.ExternalReferences);
         }
     }
 }
